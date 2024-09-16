578b908dfdebbae6efaed16f010f067338f3f73e
sankar-raul <raulsankar99@gmail.com>
2024-09-16 13:28:35 +0530

    db update

--- dbsetup.sql
+++ dbsetup.sql
@@ -1,6 +1,6 @@
-create database url_shorter;
+create database if not exists url_shorter;
 use url_shorter;
-create table urls (
+create table if not exists urls (
 	id char(6) primary key,
     url varchar(255) not null,
     visits int default 0
